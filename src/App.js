ea3b4c9b4e314670465e9c81be733b565e4b37a7
batteryhun <100200467+batteryhun@users.noreply.github.com>
2023-10-23 17:37:46 +0900

    react-master-class chapter2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from "styled-components";
+
+const rotationAnimation = keyframes`
+  0% {
+    transform:rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    transform:rotate(360deg);
+    border-radius: 100px;
+  }
+  100% {
+    transform:rotate(0deg);
+    border-radius: 0px;
+  }
+`;
+const Father = styled.div`
+  display: flex;
+`;
+const Emoji = styled.span`
+  font-size: 100px;
+`;
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  color: white;
+  background-color: ${(props) => props.bgColor};
+  animation: ${rotationAnimation} 1s linear infinite;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  ${Emoji} {
+    font-size: 100px;
+    &:hover {
+      font-size: 50px;
+    }
+  }
+`;
+
+const Circle = styled(Box)`
+  border-radius: 50%;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Father>
+      <Box bgColor="tomato">
+        <Emoji>🤗</Emoji>
+      </Box>
+      <Emoji>🤗</Emoji>
+      <Circle bgColor="teal" />
+    </Father>
   );
 }
 
