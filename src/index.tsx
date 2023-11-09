498a21fb6362bb0f4a8c66902b94a23955876603
batteryhun <100200467+batteryhun@users.noreply.github.com>
2023-11-10 03:31:11 +0900

    darkmode making

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { ThemeProvider } from "styled-components";
-import { Theme } from "./theme";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
@@ -11,9 +9,7 @@ const queryClient = new QueryClient();
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={Theme}>
-        <App />
-      </ThemeProvider>
+      <App />
     </QueryClientProvider>
   </React.StrictMode>
 );
--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,12 @@
 import { DefaultTheme } from "styled-components/dist/types";
 
-export const Theme: DefaultTheme = {
-  textColor: "#f5f6fa",
+export const darkTheme: DefaultTheme = {
+  textColor: "black",
   bgColor: "#2f3640",
+  accentColor: "#44bd32",
+};
+export const lightTheme: DefaultTheme = {
+  textColor: "black",
+  bgColor: "whitesmoke",
   accentColor: "#44bd32",
 };
